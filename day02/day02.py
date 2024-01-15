9835a53b7b1ffadf8ca8f95f4004cc47e3522f52
j.hwp <we1905@icloud.com>
2024-01-15 13:21:49 +0900

    0.6 day2

---
+++ day02/day02.py
@@ -0,0 +1,11 @@
+a = 4
+print(a)
+
+#fstring, formating
+base_number = 2
+exp_number = 5
+print(f"밑 {base_number}, 지수 {exp_number}")
+print("밑 {}, 지수 {}".format(base_number, exp_number))
+
+#c like
+print('밑은 %d, 지수는 %d' % (base_number, exp_number))
