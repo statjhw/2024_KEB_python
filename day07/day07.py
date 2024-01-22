c8595e29f1d25634f3856d5566ba40d6ed7c8df5
j.hwp <we1905@icloud.com>
2024-01-22 11:22:18 +0900

    v2.4 day07

---
+++ day07/day07.py
@@ -0,0 +1,67 @@
+#class 속성
+#클래스의 속성을 객체가 받는다. 실행시점의 클래스 속성 값으로 객체의 값이 결정된다.
+
+#Method Types
+#1. instance method : 데코레이터가 없는 메소드, self로 시작하는 변수
+#   개별적인 객체를 참조하는 함수
+#2. 클래스 메소드
+#   @classmethod
+#3. staticmethod
+#   저장되는 메모리가 다른
+
+
+#클래스 메소드 #객체가 모드 공유한다.
+#클래스 변수를 사용할 때 이용한다.
+
+
+#static 메소드
+
+
+#duck typing
+#파이썬에서 다형성을 가지는 방법
+
+
+#magic method(연산자 오버로딩)
+#원하는 작동방식으로 구현 가능
+#매개변수의 포켓몬을 넣고, 매개변수의 인수를 넣고 싶을 때 함수 오버로딩이 안되기 때문에 어떻게 구현?
+
+#클래스의 간의 연관관계
+#Aggregation and Composition
+#1. Aggregation
+
+#다중 상속 대체 가능
+#aggregation과 composition 구성 강도에 따라서
+#self.fly
+#wing객체를 만들어서 인수로 전달
+#flyclass를 상속하는 fly와 nofly 를 만든다.. overiding으로 처리 #다형성을 이용
+#setter를 이용해 class의 fly 속성을 변경해주기
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
