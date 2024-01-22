df97cc7c1f516382c8dc49b7098b6a4f7bebf7e3
jang <we1905@icloud.com>
2024-01-22 12:37:58 +0900

    v2.5 day07

--- day07/day07.py
+++ day07/day07.py
@@ -37,9 +37,60 @@
 #setter를 이용해 class의 fly 속성을 변경해주기
 
 
+#composition
+# class Fly :
+#     def fly(self):
+#         print("하늘을 난다.")
+#
+# class Pikachu :
+#     #def __init__(self, name, hp, fly):
+#     #     self.name = name
+#     #     self.hp = hp
+#     #     self.fly_behavior = fly
+#     # aggregation으로 구현
+#
+#     def __init__(self, name, hp):
+#         self.name = name
+#         self.hp = hp
+#         self.fly_behavior = Fly() #클래스 내부에서 만들어 준다. (Composition)
 
+#11장 모듈
+import myMath
+#특정 함수를 파일에 옮기고 import를 이용해 불러온다.
 
+while True:
+    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
 
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+    elif menu == '3':
+        number = int(input("Input number : "))
+        if myMath.isprime(number):
+            print(f'{number} is prime number')
+        else:
+            print(f'{number} is NOT prime number!')
+    elif menu == '4':
+        n1, n2 = map(int, input("Input first second number : ").split())
+        n1, n2 = min(n1, n2), max(n1, n2)
+        # numbers = input("Input first second number : ").split()
+        # n1 = int(numbers[0])
+        # n2 = int(numbers[1])
+        # if n1 > n2:
+        #     n1, n2 = n2, n1
+
+        for number in range(n1, n2 + 1):
+            if myMath.isprime(number):
+                print(number, end=' ')
+        print()
+    elif menu == '5':
+        print('Terminate Program.')
+        break
+    else:
+        print('Invalid Menu!')
 
 
 
